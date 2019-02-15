92d37590a3fc44d26b958f3ab86c3054ef384f3c
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-02-15 17:42:40 +0100

    update repo

--- publish/entrypoint.sh
+++ publish/entrypoint.sh
@@ -3,14 +3,14 @@
 set -e
 
 # create .npmrc
-local_registry="http://0.0.0.0:4873"
-NPM_AUTH_TOKEN="fake_token"
-VERDACCIO_CONFIG_USERCONFIG="${"$HOME/.npmrc"}"
-VERDACCIO_REGISTRY_URL="${0.0.0.0:4873}"
-VERDACCIO_REGISTRY_SCHEME="http"
-printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$VERDACCIO_REGISTRY_URL" "$NPM_AUTH_TOKEN" $local_registry > "$VERDACCIO_CONFIG_USERCONFIG"
+# local_registry="http://0.0.0.0:4873"
+# NPM_AUTH_TOKEN="fake_token"
+# VERDACCIO_CONFIG_USERCONFIG="$HOME/.npmrc"
+# VERDACCIO_REGISTRY_URL="0.0.0.0:4873"
+# VERDACCIO_REGISTRY_SCHEME="http"
+# printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$VERDACCIO_REGISTRY_URL" "$NPM_AUTH_TOKEN" $local_registry > "$VERDACCIO_CONFIG_USERCONFIG"
 
-chmod 0600 "$VERDACCIO_CONFIG_USERCONFIG"
+# chmod 0600 "$VERDACCIO_CONFIG_USERCONFIG"
 
 # Start local registry
 tmp_registry_log=`mktemp`
