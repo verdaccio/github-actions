e295f2bbebb538a97d9855afc47ea8c1c10b3437
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-02-15 17:36:22 +0100

    update repo

--- publish/entrypoint.sh
+++ publish/entrypoint.sh
@@ -3,12 +3,12 @@
 set -e
 
 # create .npmrc
-NPM_AUTH_TOKEN="test_token" 
-VERDACCIO_CONFIG_USERCONFIG="${VERDACCIO_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-VERDACCIO_REGISTRY_URL="${VERDACCIO_REGISTRY_URL-0.0.0.0:4873}"
-VERDACCIO_STRICT_SSL="${VERDACCIO_STRICT_SSL-false}"
+local_registry="http://0.0.0.0:4873"
+NPM_AUTH_TOKEN="fake_token"
+VERDACCIO_CONFIG_USERCONFIG="${"$HOME/.npmrc"}"
+VERDACCIO_REGISTRY_URL="${0.0.0.0:4873}"
 VERDACCIO_REGISTRY_SCHEME="http"
-printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$VERDACCIO_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${VERDACCIO_REGISTRY_SCHEME}://$VERDACCIO_REGISTRY_URL" "${VERDACCIO_STRICT_SSL}" > "$VERDACCIO_CONFIG_USERCONFIG"
+printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$VERDACCIO_REGISTRY_URL" "$NPM_AUTH_TOKEN" $local_registry "${VERDACCIO_STRICT_SSL}" > "$VERDACCIO_CONFIG_USERCONFIG"
 
 chmod 0600 "$VERDACCIO_CONFIG_USERCONFIG"
 
@@ -20,7 +20,6 @@ sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_regis
 # Wait for `verdaccio` to boot
 # grep -q 'http address' <(tail -f $tmp_registry_log)
 # Login so we can publish packages
-sh -c "npm set registry https://registry.npmjs.org/"
-sh -c "npx npm-auth-to-token@1.0.0 -u user -p password -e user@example.com -r http://0.0.0.0:4873"
+sh -c "npm-auth-to-token@1.0.0 -u test -p test -e test@test.com -r $local_registry"
 # Run nmp command
-sh -c "npm --registry http://0.0.0.0:4873 publish $1"
+sh -c "npm --registry $local_registry publish $1"
