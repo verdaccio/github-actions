715805d9acbc3c673af0549424ed6c0b20de9009
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-02-15 09:25:27 +0100

    update repo

--- publish/entrypoint.sh
+++ publish/entrypoint.sh
@@ -6,12 +6,8 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect VERDACCIO_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   VERDACCIO_CONFIG_USERCONFIG="${VERDACCIO_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
   VERDACCIO_REGISTRY_URL="${VERDACCIO_REGISTRY_URL-localhost:4873}"
-  VERDACCIO_STRICT_SSL="${VERDACCIO_STRICT_SSL-true}"
-  VERDACCIO_REGISTRY_SCHEME="https"
-  if ! $VERDACCIO_STRICT_SSL
-  then
-    VERDACCIO_REGISTRY_SCHEME="http"
-  fi
+  VERDACCIO_STRICT_SSL="${VERDACCIO_STRICT_SSL-false}"
+  VERDACCIO_REGISTRY_SCHEME="http"
 
   printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$VERDACCIO_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${VERDACCIO_REGISTRY_SCHEME}://$VERDACCIO_REGISTRY_URL" "${VERDACCIO_STRICT_SSL}" > "$VERDACCIO_CONFIG_USERCONFIG"
 
@@ -22,4 +18,4 @@ sh -c "verdaccio --config /verdaccio/config.yaml &"
 
 cat "$HOME/.npmrc"
 
-sh -c "npm  --access public --registry http://localhost:4873 $1"
+sh -c "npm --registry http://localhost:4873 $1 --access public"
