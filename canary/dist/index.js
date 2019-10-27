7616c8a121c56cb53d594c233770f59801aaaf4a
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-10-27 12:55:36 +0100

    update canary

--- canary/dist/index.js
+++ canary/dist/index.js
@@ -564,7 +564,9 @@ async function run() {
         owner,
         repo,
         pull_number: number,
-        body: `Thanks for your PR, we have promoted your PR and created a canary version of your PR: \`\`\`npm install --global verdaccio@${outputExec} --registry https://registry.verdaccio.org  \`\`\``,
+        body: `Thanks for your PR, we have promoted your PR and created a canary version of your PR:
+
+        \`\`\`npm install --global verdaccio@${outputExec} --registry https://registry.verdaccio.org  \`\`\``,
         event: 'COMMENT'
       });
   }
