852b39850f0e99dca3f0fb422713ba5b4184e3c2
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-10-27 11:15:27 +0100

    Update index.js

--- canary/index.js
+++ canary/index.js
@@ -20,10 +20,21 @@ async function run() {
         core.getInput('repo-token', {required: true})
       );
       const context = github.context;
+      core.debug(`action: ${context.payload.action}`);
+      core.debug(`payload: ${JSON.stringify(context.payload, null, 2)}`);
       const {owner, repo, number} = context.issue;
+      const issueNumber = context.payload.pull_request.number;
       core.debug(`owner: ${owner}`);
       core.debug(`number: ${number}`);
       core.debug(`repo: ${repo}`)
+      client.pulls.createComment({
+        owner,
+        repo,
+        pull_number: number,
+        body: 'hey thanks for your PR',
+        commit_id: '',
+        path: 'README.md'
+      })
   }
   catch (error) {
     core.setFailed(error.message);
