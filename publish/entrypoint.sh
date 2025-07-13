67cc17100b8b0a543eb47a699609565bb373cbb9
Juan Picado <juanpicado19@gmail.com>
2025-07-13 17:10:21 +0200

    chore: replace library

--- publish/entrypoint.sh
+++ publish/entrypoint.sh
@@ -13,6 +13,6 @@ sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_regis
 # FIXME: this throws a syntax error, but would be great to make it run
 # grep -q 'http address' <(tail -f $tmp_registry_log)
 # login so we can publish packages
-sh -c "npm-auth-to-token -u test -p test -e test@test.com -r $local_registry"
+sh -c "npx npm-cli-login -u test -p test -e test@domain.test -r http://localhost:4873$local_registry"
 # Run nmp command
 sh -c "npm publish --registry $local_registry $1"
